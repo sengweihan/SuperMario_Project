5c4ea1c63568889c9cd5584087089a52f012094a
SengWeiHan <wsen0004@student.monash.edu>
2022-04-28 22:05:43 +0800

    Added Pickup Coin Action class and a new interface called PickableCoin

---
+++ src/game/interfaces/PickableCoin.java
@@ -0,0 +1,9 @@
+package game.interfaces;
+
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+
+public interface PickableCoin {
+
+    String pickableCoin(Actor actor, GameMap map);
+}
